48e1bf4bd524d44f76478f36a7a625f25d9d2237
kishannarote <kishannnarote@gmail.com>
2020-09-05 09:51:22 -0700

    First time commit on git

---
+++ BasicCoreJava/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,21 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		
+		int age=20;
+		String name="kishan";
+		char gender='M';
+		boolean maritstatus=true;
+		double height=5.8;
+		
+		
+		System.out.println(age);
+		
+		
+		
+	}
+
+}
---
+++ BasicCoreJava/src/variablesAndDataTypes/MyFirst.java
@@ -0,0 +1,11 @@
+package variablesAndDataTypes;
+
+public class MyFirst {
+
+	public static void main(String[] args) {
+	
+		System.out.println("hello");
+	
+	}
+
+}
